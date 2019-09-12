eb2369628305d83595b3b511308b0541fa3252d5
LenWinkler <lenwinkler86@gmail.com>
2019-09-12 15:46:40 -0400

    refactored Nav using styled-components

--- src/Header/Nav.js
+++ src/Header/Nav.js
@@ -1,15 +1,35 @@
 import React from "react";
 import "./Nav.css";
+import styled from "styled-components";
+
+const Link = styled.a`
+  font-size: 1.5rem;
+  text-decoration: none;
+  margin: .8rem;
+  margin-right: 4.8rem;
+`;
+
+const NavBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Date = styled.p`
+  font-size: 1.5rem;
+  margin-left: 1.8rem;
+`;
+
 
 const Nav = (props) => {
     var date = props.date;
     return (
-      <div className="navBar">
-        <p className="date">{date}</p>
-        <div className="divLinks">
-            <a className="link" href="https://github.com/LenWinkler/nasa-photo-of-the-day/tree/len-winkler">View Code</a>
+      <NavBar>
+        <Date>{date}</Date>
+        <div>
+            <Link href="https://github.com/LenWinkler/nasa-photo-of-the-day/tree/len-winkler">View Code</Link>
         </div>
-      </div>  
+      </NavBar>  
     )
 }
 
